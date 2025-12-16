6a685e2d08dd990f8d8d586d7583b1bd9cf94936
njlmelindakst <najlamelindakiasati@gmail.com>
2025-12-16 23:17:51 +0700

    Update main.py

--- UAS/main.py
+++ UAS/main.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import pandas as pd 
 
-st.title("KANDEP")
+#Title
+st.title("TENANT KANDEP")
+st.write("Rekomendasi dari TOP 3 Program Studi terbanyak")
+st.markdown("---")
+
+menu = st.sidebar.selectbox(
+    "Pilih Halaman",
+    ["Beranda","About us"]
+)
+
+st.subheader("Informatika")
+
+if menu == "Beranda":
+    st
